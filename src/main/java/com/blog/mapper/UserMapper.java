bf841077bf7a37eb92771ee182a36316c1cd7ce3
fdse <2795656376@qq.com>
2023-04-21 23:30:33 +0800

    --story=[ID1018873] --user=[宋卓] 登录注册pojo,mapper层代码完成

---
+++ src/main/java/com/blog/mapper/UserMapper.java
@@ -0,0 +1,60 @@
+package com.blog.mapper;
+
+
+import com.blog.pojo.Select;
+import com.blog.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+//记得@mapper
+@Repository
+@Mapper
+public interface UserMapper {
+
+    String getAdminPassword();
+
+    int getTotalUserCount();
+
+    User getUserByUsername(String username);
+
+    List<User> getUserList();
+
+    User getUserById(@Param("id") Integer id);
+
+    List<User> getUserByWhich(Select select);
+
+    int getUserByWhichCount(Select select);
+
+    void deleteUserById(@Param("id") Integer id);
+
+    void updateUserById(User user);
+
+    void insertUser(User user);
+
+    void topicCountPlus(Integer id);
+
+    void commentCountPlus(Integer id);
+
+    void browseCountPlus(Integer id);
+
+    void supportCountPlus(Integer id);
+
+    void supportCountMinus(Integer id);
+
+    void topicCountMinus(Integer id);
+
+    List<User> getHotCommentUser(int count);
+
+    void updateUserLimit(User user);
+
+    User getUserByEmail(String email);
+
+    List<User> getUserListByMessage(String namMessage);
+
+    void updateOnlyOne(@Param("id") Integer id, @Param("value") String value, @Param("message") String message);
+
+
+}
---
+++ src/main/java/com/blog/pojo/User.java
@@ -0,0 +1,25 @@
+package com.blog.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String avatar;
+    private String introduction;
+    private int topicCount;
+    private int commentCount;
+    private int rankId;
+    private int supportCount;
+    private int rankLevel;
+    private int sex;
+    private int browseCount;
+    private int secret;
+}
